610d622c1c94b86fe512440b443f68b9c481d89e
Kameltalbi <kameltalbi.tn@gmail.com>
2025-08-14 17:31:08 +0100

    Correction de l'import dupliqué dans Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { ArrowRight, Download, BarChart3 } from 'lucide-react';
 
 const Hero = () => {
